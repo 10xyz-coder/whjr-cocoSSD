7d532599644f49e3ca4bdd36dad696c40c3147f9
10xyz <82887902+10xyz-coder@users.noreply.github.com>
2023-06-10 15:41:02 +0530

    Update mini.js v2

--- mini.js
+++ mini.js
@@ -7,6 +7,8 @@ g('b').onclick = (() => {
 })
 
 let canvas;
+let res;
+let mainImg;
 
 function preload() {
   // Load the image before the sketch starts
@@ -34,15 +36,26 @@ function setup() {
 
   // Display the image on the canvas
   image(img, 0, 0, scaledWidth, scaledHeight);
+  mainImg = img;
   if (location.href.includes('cat')) {
     image(img4, 0, 0, scaledWidth, scaledHeight);
+    mainImg = img4;
   }
   if (location.href.includes('ball')) {
     image(img2, 0, 0, scaledWidth, scaledHeight);
+    mainImg = img2;
   }
   if (location.href.includes('mobile')) {
     image(img3, 0, 0, scaledWidth, scaledHeight);
+    mainImg = img3;
   }
+
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+}
+
+function modelLoaded() {
+  console.log('Model Loaded!');
+  objectDetector.detect(mainImg, gotResult);
 }
 
 function windowResized() {
@@ -50,4 +63,32 @@ function windowResized() {
   const centerX = (windowWidth - width) / 2;
   const centerY = (windowHeight - height) / 2;
   canvas.position(centerX, centerY);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    res = results;
+    drawResults(res)
+  }
+}
+
+function drawResults(r) {
+  for (i = 0; i < r.length; i++) {
+
+    let label = r[i].label;
+    let conf = r[i].confidence;
+    let x = r[i].x;
+    let y = r[i].y;
+    let height = r[i].height;
+    let width = r[i].width;
+
+    fill('#FF0000');
+    text(label, x + 45, y + 75)
+    noFill()
+    stroke('#FF0000');
+    rect(x, y, width, height)
+  }
 }
